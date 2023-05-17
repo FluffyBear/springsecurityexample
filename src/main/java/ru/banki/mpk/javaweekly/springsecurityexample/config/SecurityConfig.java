8c250892bd592a8dc648ba4f4b32f5fa145706dd
Maksimenko Aleksandr <maksimenko.fil-it@bankismart.com>
2023-05-17 12:30:09 +0300

    step4 - can filter by authorities

--- src/main/java/ru/banki/mpk/javaweekly/springsecurityexample/config/SecurityConfig.java
+++ src/main/java/ru/banki/mpk/javaweekly/springsecurityexample/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.banki.mpk.javaweekly.springsecurityexample.model.Permission;
 import ru.banki.mpk.javaweekly.springsecurityexample.model.Role;
 
 @Configuration
@@ -21,9 +22,9 @@ public class SecurityConfig {
                 .csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
-                .requestMatchers(HttpMethod.POST, "/api/**").hasRole(Role.ADMIN.name())
-                .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole(Role.ADMIN.name())
+                .requestMatchers(HttpMethod.GET, "/api/**").hasAuthority(Permission.DEVELOPERS_READ.getPermission())
+                .requestMatchers(HttpMethod.POST, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
+                .requestMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(Permission.DEVELOPERS_WRITE.getPermission())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -36,11 +37,11 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(
                 User.builder().username("bankiru")
                         .password(passwordEncoder().encode("bankiru"))
-                        .roles(Role.ADMIN.name())
+                        .authorities(Role.ADMIN.getAuthorities())
                         .build(),
                 User.builder().username("user")
                         .password(passwordEncoder().encode("user"))
-                        .roles(Role.USER.name())
+                        .authorities(Role.USER.getAuthorities())
                         .build()
         );
     }
