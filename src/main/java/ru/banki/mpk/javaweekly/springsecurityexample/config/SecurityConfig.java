1da2638387c7120fd0192b22aa134517c686a5e8
Maksimenko Aleksandr <maksimenko.fil-it@bankismart.com>
2023-05-17 16:49:49 +0300

    step8 - update config & fixes

--- src/main/java/ru/banki/mpk/javaweekly/springsecurityexample/config/SecurityConfig.java
+++ src/main/java/ru/banki/mpk/javaweekly/springsecurityexample/config/SecurityConfig.java
@@ -3,30 +3,50 @@ package ru.banki.mpk.javaweekly.springsecurityexample.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.banki.mpk.javaweekly.springsecurityexample.repository.UserRepository;
 import ru.banki.mpk.javaweekly.springsecurityexample.security.JwtConfigurer;
+import ru.banki.mpk.javaweekly.springsecurityexample.security.UserDetailsServiceImpl;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtConfigurer jwtConfigurer;
+    private final UserRepository userRepository;
 
     @Autowired
-    public SecurityConfig(final JwtConfigurer jwtConfigurer) {
+    public SecurityConfig(final JwtConfigurer jwtConfigurer,
+                          final UserRepository userRepository) {
         this.jwtConfigurer = jwtConfigurer;
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    public AuthenticationManager authManager() {
+        final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(new UserDetailsServiceImpl(userRepository));
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(authProvider);
     }
 
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeHttpRequests()
                 .requestMatchers("/").permitAll()
+                .requestMatchers("/api/v1/auth/login").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
